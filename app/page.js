2dc4860e86b4ec8eaf63c884f3fa228e18dda503
Dron245 <Dron245@yandex.ru>
2024-11-21 20:06:54 +0300

    2111-2006

--- app/page.js
+++ app/page.js
@@ -1,10 +1,22 @@
 import Link from "next/link";
-export default function Home() {
+
+export default async function Home() {
+	const data = await fetch('https://dummyjson.com/posts')
+	const resp = await data.json()
+	const posts = resp.posts
+	
 	return (
 		<div className="container test">
 			<h1>Привет</h1>
-			<Link href="/client">Клиент</Link><br/>
-			<Link href="/client/show">show</Link>
+			{/* <Link href="/client">Клиент</Link><br/> */}
+			{/* <Link href="/client/show">show</Link> */}
+			{posts.map(post=>(
+				<div className="posts">
+					<h2 className="posts__title">{post.title}</h2>
+					<p className="posts__text">{post.body}</p>
+					<Link className="posts__link" href={`/posts + ${post.id}`}>Подробнее</Link>
+				</div>
+			))}
 		</div>
 	);
 }
